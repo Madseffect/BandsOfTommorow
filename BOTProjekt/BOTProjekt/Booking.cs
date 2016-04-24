95de0e25e7bc8e1c1dc731db3b4fd31dd2397007
mikkel madsen <aebeloe.madsen@hotmail.com>
2016-04-24 10:57:05 +0200

    Testcommit2

--- BOTProjekt/BOTProjekt/Booking.cs
+++ BOTProjekt/BOTProjekt/Booking.cs
@@ -6,21 +6,22 @@ using System.Threading.Tasks;
 
 namespace BOTProjekt
 {
-    class Booking
+    internal class Booking
     {
-        public string  Lokale  { get; set; }
+        public string Lokale { get; set; }
         public string Kursus { get; set; }
 
         public Booking(string lokale, string kursus)
         {
             Lokale = lokale;
             Kursus = kursus;
-        
 
-    }
+
+        }
 
         public override string ToString()
         {
             return $"Lokale: {Lokale}, Kursus: {Kursus}";
         }
     }
+}
