a817f68b56fb89033878c08d13313a43585c273f
mikkel madsen <aebeloe.madsen@hotmail.com>
2016-04-24 10:37:22 +0200

    First Commit

---
+++ BOTProjekt/BOTProjekt/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOTProjekt
+{
+    class Booking
+    {
+        public string  Lokale  { get; set; }
+        public string Kursus { get; set; }
+
+        public Booking(string lokale, string kursus)
+        {
+            Lokale = lokale;
+            Kursus = kursus;
+        }
+    }
+}
